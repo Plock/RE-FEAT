748169ad3ecebf8a72fdac0235fcfe383f39dcb7
Cutlass <kwonarseus@naver.com>
2018-12-10 13:45:58 +0900

    add public, views

---
+++ public/scripts/audio.js
@@ -0,0 +1,80 @@
+function $(id) { return document.getElementById(id); };
+const media = document.getElementById('audio');
+
+let ui = {
+    play: 'playAudio',
+    audio: 'audio',
+    percentage: 'percentage',
+    seekObj: 'seekObj',
+    currentTime: 'currentTime'
+    };
+
+let playAudios = document.querySelectorAll(".playAudio");
+
+function togglePlay(e) {
+    if (media.paused === false) {
+        media.pause();
+        for (let playAudio of playAudios) {
+            playAudio.classList.remove('pause');
+        }
+    } else {
+        for (let playAudio of playAudios) {
+            if (e.target.id == "1") {
+                (document.querySelector("audio")).src = "../public/music/1opthe-dayop.mp3";
+            } else if (e.target.id == "2") {
+                (document.querySelector("audio")).src = "../public/music/21opop.mp3";
+            } else if (e.target.id == "3") {
+                (document.querySelector("audio")).src = "../public/music/mvlemon.mp3";
+            } else if (e.target.id == "4") {
+                (document.querySelector("audio")).src = "../public/music/little-glee-monster-sekai-ha-anatani-waraikaketeiru.mp3";
+            } else {
+                (document.querySelector("audio")).src = "../public/music/mr-twin-sister-lady-daydream.mp3";
+            }
+            media.play();
+            playAudio.classList.add('pause');
+        }
+    }
+}
+
+function calculatePercentPlayed() {
+    let percentage = (media.currentTime / media.duration).toFixed(2) * 100;
+    $(ui.percentage).style.width = `${percentage}%`;
+}
+
+function calculateCurrentValue(currentTime) {
+    const currentMinute = parseInt(currentTime / 60) % 60;
+    const currentSecondsLong = currentTime % 60;
+    const currentSeconds = currentSecondsLong.toFixed();
+    const currentTimeFormatted = `${currentMinute < 10 ? `0${currentMinute}` : currentMinute}:${
+        currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds
+    }`;
+
+    return currentTimeFormatted;
+}
+
+function initProgressBar() {
+    const currentTime = calculateCurrentValue(media.currentTime);
+    $(ui.currentTime).innerHTML = currentTime;
+    $(ui.seekObj).addEventListener('click', seek);
+
+    media.onended = () => {
+        for (let playAudio of playAudios) {
+            playAudio.classList.remove('pause');
+            $(ui.percentage).style.width = 0;
+            $(ui.currentTime).innerHTML = '00:00';
+        }
+    };
+
+    function seek(e) {
+        const percent = e.offsetX / this.offsetWidth;
+        media.currentTime = percent * media.duration;
+    }
+
+    calculatePercentPlayed();
+}
+
+let plays = document.querySelectorAll(".playAudio");
+for (let play of plays) {
+    play.addEventListener('click', togglePlay)
+    $(ui.audio).addEventListener('timeupdate', initProgressBar);
+}
