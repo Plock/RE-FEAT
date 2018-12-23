da5ebe6bb290dc60fb864bee63041d2b6a0f452f
Cutlass <kwonarseus@naver.com>
2018-12-23 18:05:28 +0900

    많은것을 수정

---
+++ app.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const createError = require('http-errors');
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: '#$#$Project #$$#RE:FEAT$##$',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
