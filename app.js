2934489d4d33dd41c61738337a41bfa46dc81dab
Cutlass <kwonarseus@naver.com>
2018-12-23 21:11:38 +0900

    환상의 커밋쇼. 무엇인가 바뀌었습니다!

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
 
 const app = express();
 
@@ -24,9 +27,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: '#$#$Project #$$#RE:FEAT$##$',
@@ -34,7 +35,23 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+passport.use(new LocalStrategy(
+  ((username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }),
+));
+
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res) => {
---
+++ routes/login.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('login', { title: 'RE:FEAT' });
+});
+
+router.post('/passport', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+module.exports = router;
